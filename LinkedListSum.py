3864b4ce6d0a2a63b1e7f8284badf56600d24219
Sunilsai1066 <sunilsai6268@gmail.com>
2021-12-22 22:45:53 +0530

    Basic Program To Find Sum Of Linked List

---
+++ LinkedListSum.py
@@ -0,0 +1,22 @@
+class Node:
+    def __init__(self,val):
+        self.val = val
+        self.next = None
+
+def LinkedListSum(head):
+    Sum = 0
+    while(head != None):
+        Sum += head.val
+        head = head.next
+    return Sum
+
+Link1 = Node(2)
+Link2 = Node(5)
+Link3 = Node(7)
+Link4 = Node(9)
+
+Link1.next = Link2
+Link2.next = Link3
+Link3.next = Link4
+
+print(LinkedListSum(Link1))
